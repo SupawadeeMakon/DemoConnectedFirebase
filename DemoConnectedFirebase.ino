77a897c6e74696e53c8cc36ac5eedc13219eab20
Supawadee <supawadee018@gmail.com>
2018-01-24 14:07:49 +0700

    Connected WiFi

--- DemoConnectedFirebase.ino
+++ DemoConnectedFirebase.ino
@@ -1,14 +1,53 @@
+//Add Library
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
 
-//Explicit
+
+
+
+//Explicit ประกาศตัวแปร
+
+//การกำหนดค่าคงที
+#define wifiSSID "MastreEWTC_2.4G"    //String wifiSSID="";
+#define wifiPassword "12345abcde"
 
 
 
 void setup() {
-  // put your setup code here, to run once:
+  
+//  การกำหนดช่องทางในการ monitor
+  Serial.begin(9600);
+
+//  การกำหนดข้อมูลเกี่ยวกับการเชื่อมต่อ wifi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifiSSID, wifiPassword);
+  Serial.println("Start Find WiFi");  
+
+  while(WiFi.status() != WL_CONNECTED) 
+  {
+    //ถ้า Condition Ture จะทำงานที่นี้
+    Serial.print("=");
+    delay(1000);
+    
+    }
+
+//    ถ้ามาทำงานตรงนี้คือ Connected WiFi ได้แล้ว
+  Serial.println();
+  Serial.println("Success Connected WiFi");
+  Serial.println(WiFi.localIP());
 
 }//setup
 
 void loop() {
-  // put your main code here, to run repeatedly:
+//  Serial.println("Hello Supawadee");
+//  delay(2000);
 
+
+
+
+
+
+  
 }//Loop
+
+
