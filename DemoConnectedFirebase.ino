99b7254311b2dd4f7be4e84de30d38bc22bdde87
Supawadee <supawadee018@gmail.com>
2018-01-24 14:56:20 +0700

    Update Firebase

--- DemoConnectedFirebase.ino
+++ DemoConnectedFirebase.ino
@@ -2,14 +2,18 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
-
-
-
 //Explicit ประกาศตัวแปร
 
 //การกำหนดค่าคงที
 #define wifiSSID "MastreEWTC_2.4G"    //String wifiSSID="";
 #define wifiPassword "12345abcde"
+//-------------------------------------------------
+//การทำงานบนfirebase
+#define firebaseHost "supawadeenodemcu.firebaseio.com"
+#define firebaseKey  "q6iIgeARHkKbklWfTpUZ3mziiPaptX8XzS0yp6cr"
+//-------------------------------------------------
+int intIndex=0;
+
 
 
 
@@ -17,7 +21,7 @@ void setup() {
   
 //  การกำหนดช่องทางในการ monitor
   Serial.begin(9600);
-
+//-------------------------------------------------
 //  การกำหนดข้อมูลเกี่ยวกับการเชื่อมต่อ wifi
   WiFi.mode(WIFI_STA);
   WiFi.begin(wifiSSID, wifiPassword);
@@ -36,15 +40,34 @@ void setup() {
   Serial.println("Success Connected WiFi");
   Serial.println(WiFi.localIP());
 
+//------------------------------------------------------
+//  ส่วนของการเชื่อมต่อ Firebase 
+
+  Firebase.begin(firebaseHost, firebaseKey);
+
 }//setup
 
 void loop() {
 //  Serial.println("Hello Supawadee");
 //  delay(2000);
 
+//Check Connected Firebase
+  Firebase.setInt("myTimes", intIndex);
+  
+  if (Firebase.failed()) {
 
+    //    เมื่อไรก็ตามที่ไม่สามารถเข้า Firebase
+    Serial.print("Connot Connected Firebase :...");
+    Serial.println(Firebase.error());
+    return;
+    
+    }
 
+    Serial.print("Success Connected Firebase Times ==>");
+    Serial.println(Firebase.getInt("myTimes"));
 
+    intIndex++;
+    delay(500);
 
 
   
